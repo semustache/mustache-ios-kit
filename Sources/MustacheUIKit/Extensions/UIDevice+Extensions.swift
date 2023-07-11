238aaadf2a3f4954b3a5a47ff3c914211accc3ca
Tommy Sadiq Hinricshen <th@mustache.dk>
2023-07-11 21:02:27 +0200

    Added model identifier to UIKit

---
+++ Sources/MustacheUIKit/Extensions/UIDevice+Extensions.swift
@@ -0,0 +1,17 @@
+
+import UIKit
+
+extension UIDevice {
+    
+    var modelIdentifier: String? {
+        if let simulatorModelIdentifier = ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] {
+            return simulatorModelIdentifier
+        }
+        var sysinfo = utsname()
+        uname(&sysinfo) // ignore return value
+        let machine = Data(bytes: &sysinfo.machine, count: Int(_SYS_NAMELEN))
+        let modelIdentifier = String(bytes: machine, encoding: .ascii)?.trimmingCharacters(in: .controlCharacters)
+        return modelIdentifier
+    }
+    
+}
