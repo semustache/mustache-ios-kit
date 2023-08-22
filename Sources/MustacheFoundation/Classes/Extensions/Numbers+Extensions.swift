2f9f66f56dd77f01c74a31391d01296bcfb57e99
Tommy Sadiq Hinricshen <th@mustache.dk>
2023-08-22 09:28:24 +0200

    Added rounded to double

--- Sources/MustacheFoundation/Classes/Extensions/Numbers+Extensions.swift
+++ Sources/MustacheFoundation/Classes/Extensions/Numbers+Extensions.swift
@@ -14,6 +14,13 @@ public extension Double {
     func format(format: String) -> String {
         return String(format: "%\(format)f", self)
     }
+    
+    /// Rounds the double to decimal places value
+    func rounded(toPlaces places: Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+    
 }
 
 public extension Int {
