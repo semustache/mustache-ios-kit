ea6da0bddecd9ddc9280f765ec3629e967ef3361
Tommy Sadiq Hinricshen <th@mustache.dk>
2023-11-08 12:56:17 +0100

    Added hex constructor to uicolor

--- Sources/MustacheUIKit/Extensions/UIColor+Extensions.swift
+++ Sources/MustacheUIKit/Extensions/UIColor+Extensions.swift
@@ -4,18 +4,39 @@ import UIKit
 import MustacheFoundation
 
 public extension UIColor {
-
-  var colorAsUInt32: UInt {
-       var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
-       self.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-
-       var colorAsUInt32: UInt32 = 0
-       colorAsUInt32 += UInt32(red * 255.0) << 16
-       colorAsUInt32 += UInt32(green * 255.0) << 8
-       colorAsUInt32 += UInt32(blue * 255.0)
-
-       let colorAsUInt = UInt(colorAsUInt32)
-       return colorAsUInt
-   }
-
+    
+    convenience init(hex: String) {
+        
+        var string = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        
+        var int = UInt64()
+        Scanner(string: string).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch string.count {
+            case 3: // RGB (12-bit)
+                (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+            case 6: // RGB (24-bit)
+                (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+            case 8: // ARGB (32-bit)
+                (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+            default:
+                (a, r, g, b) = (255, 1, 1, 1)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+        
+    }
+    
+    var colorAsUInt32: UInt {
+        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
+        self.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        var colorAsUInt32: UInt32 = 0
+        colorAsUInt32 += UInt32(red * 255.0) << 16
+        colorAsUInt32 += UInt32(green * 255.0) << 8
+        colorAsUInt32 += UInt32(blue * 255.0)
+        
+        let colorAsUInt = UInt(colorAsUInt32)
+        return colorAsUInt
+    }
+    
 }
